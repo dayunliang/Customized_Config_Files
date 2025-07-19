042c1eb0d32705c9d4febd7cedc685dcd1b32b66
dayunliang <36461319+dayunliang@users.noreply.github.com>
2025-07-19 17:07:20 +0800

    Update back-route-cron.sh

--- Lean/files/usr/bin/back-route-cron.sh
+++ Lean/files/usr/bin/back-route-cron.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export PATH=/usr/sbin:/usr/bin:/sbin:/bin
+
 # 参数配置（可按需修改）
 NET_CIDR="192.168.122.0/24"
 TABLE_NAME="backroute"
@@ -7,15 +9,18 @@ TABLE_ID="100"
 GATEWAY="192.168.12.254"
 IFACE="eth1"
 
-# 检查策略路由是否存在
-ip rule | grep -q "from $NET_CIDR lookup $TABLE_NAME"
+# 日志输出当前环境
+logger -t backroute-cron "检测ip rule: $(ip rule list)"
+logger -t backroute-cron "检测ip route: $(ip route show table $TABLE_ID)"
+
+# 检查策略路由是否存在（兼容 lookup 名字或数字）
+ip rule | grep -Eq "from $NET_CIDR lookup ($TABLE_ID|$TABLE_NAME)"
 RULE_EXIST=$?
 
 # 检查回程表是否存在默认路由
 ip route show table $TABLE_ID | grep -q "default via $GATEWAY dev $IFACE"
 ROUTE_EXIST=$?
 
-# 执行判断
 if [ $RULE_EXIST -ne 0 ] || [ $ROUTE_EXIST -ne 0 ]; then
   logger -t backroute-cron "❗发现缺失规则，执行修复脚本..."
   /usr/bin/back-route-complete.sh
