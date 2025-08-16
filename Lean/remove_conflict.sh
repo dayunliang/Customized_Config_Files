9488deae419978cd640e1a2551050e7ee4bf19bc
dayunliang <36461319+dayunliang@users.noreply.github.com>
2025-08-16 18:03:38 +0800

    Update remove_conflict.sh

--- Lean/remove_conflict.sh
+++ Lean/remove_conflict.sh
@@ -16,4 +16,4 @@ rm -rf package/feeds/istore/luci-app-LingTiGameAcc
 
 # 确保 ssr-plus 不再依赖 dns2socks-rust
 # 自动注释掉那一行
-sed -i 's/^\s*+PACKAGE_$(PKG_NAME)_INCLUDE_DNS2SOCKS_RUST/#&/' feeds/small/luci-app-ssr-plus/Makefile
+sed -i 's/^\s*+PACKAGE_$(PKG_NAME)_INCLUDE_DNS2SOCKS_RUST.*/#& \\/' feeds/small/luci-app-ssr-plus/Makefile
