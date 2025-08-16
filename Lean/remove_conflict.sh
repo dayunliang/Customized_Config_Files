3c7fbb16530aff0c2119522c4b8a265a7dedc1d1
dayunliang <36461319+dayunliang@users.noreply.github.com>
2025-08-16 15:35:27 +0800

    Create remove_conflict.sh

---
+++ Lean/remove_conflict.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# 删除/屏蔽掉编译中用不到、会引发循环依赖的包
+
+# Passwall / Bypass / Fchomo
+rm -rf feeds/small/luci-app-bypass
+rm -rf feeds/small/luci-app-passwall
+rm -rf feeds/small/luci-app-passwall2
+rm -rf feeds/small/luci-app-fchomo
+
+# Baresip + Apps
+rm -rf feeds/telephony/net/baresip
+rm -rf package/feeds/telephony/baresip-apps
+
+# LingTiGameAcc
+rm -rf package/feeds/istore/luci-app-LingTiGameAcc
