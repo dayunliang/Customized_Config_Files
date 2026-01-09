95dcd606b7da1b1188b80376183f66dda34d8f81
dayunliang <36461319+dayunliang@users.noreply.github.com>
2026-01-09 16:34:58 +0800

    Update config.js

--- subconverter/subweb/app/public/conf/config.js
+++ subconverter/subweb/app/public/conf/config.js
@@ -28,44 +28,6 @@ window.config = {
       value: 'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full.ini',
       text: 'ACL4SSR Online Full',
     },
-  ],
-};
-/app/public/conf # exit
-localhost:~/subconverter# docker exec -it subweb sh
-/ # cd ./app/public/conf/
-/app/public/conf # ls
-config.js
-/app/public/conf # cat config.js 
-window.config = {
-  // 网站标题
-  siteName: 'Subconverter Web',
-  // API 地址
-  apiUrl: 'http://127.0.0.1:25500',
-  // 短域名服务地址
-  shortUrl: 'https://s.ops.ci',
-  // 首页菜单
-  menuItem: [
-    {
-      title: '首页',
-      link: '/',
-      target: '',
-    },
-    {
-      title: 'GitHub',
-      link: 'https://github.com/stilleshan/subweb',
-      target: '_blank',
-    },
-  ],
-  // 远程配置地址,可以自行按照格式添加。
-  remoteConfigOptions: [
-    {
-      value: 'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online.ini',
-      text: 'ACL4SSR Online',
-    },
-    {
-      value: 'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full.ini',
-      text: 'ACL4SSR Online Full',
-    },
     {
       value: 'https://raw.githubusercontent.com/dayunliang/OpenClash/main/ACL4SSR_Online_Full_AJ_Home.ini',
       text: 'ACL4SSR Online Full AJ Home',
